7673d395a3375577e6ef83614c7da7ea8adaffa2
mohamedsalah1224 <loding600@gmail.com>
2023-12-19 19:31:18 +0200

    Add FacebookRepositoryService , Plugin

--- lib/Service/Firebase/social_service/repository_implementaion_service/facebook_repository_service.dart
+++ lib/Service/Firebase/social_service/repository_implementaion_service/facebook_repository_service.dart
@@ -1,16 +1,42 @@
+import 'package:flutter/material.dart';
 import 'package:quizzy_app/Service/Firebase/social_service/repository/social_repository.dart';
 import 'package:quizzy_app/model/social_service_response_model.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class FacebookRepositoryService implements SocialRepository {
   @override
-  Future<SocialServiceResponseModel> login() {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<SocialServiceResponseModel> login() async {
+    try {
+      final LoginResult loginResult = await FacebookAuth.instance.login();
+      if (loginResult.status == LoginStatus.success) {
+        // you are logged
+        final AccessToken accessToken = loginResult.accessToken!;
+
+        return SocialServiceResponseModel(
+            status: true,
+            providerId: accessToken.applicationId,
+            providertype: 'facebook',
+            message: 'Login  Successful with Facebook');
+      } else {
+        debugPrint(loginResult.message.toString());
+        return SocialServiceResponseModel(
+            status: false,
+            message:
+                'Login Error with Facebook \n reason:${loginResult.message.toString()}');
+      }
+    } catch (e) {
+      return SocialServiceResponseModel(
+          status: false, message: 'Login Error with Facebook');
+    }
   }
 
   @override
-  Future<bool> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
+  Future<bool> logout() async {
+    try {
+      await FacebookAuth.instance.logOut();
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 }
