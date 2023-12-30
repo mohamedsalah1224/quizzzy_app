24a6adc36a0c0cb53557ddf295992716623fc03f
mohamedsalah1224 <loding600@gmail.com>
2023-12-30 16:25:53 +0200

    update to ExamConstatnt

--- lib/utils/constant/exam_/costant.dart
+++ lib/utils/constant/exam_/costant.dart
@@ -1,9 +1,14 @@
-class ExamTypeConstatnt {
-  const ExamTypeConstatnt._();
+class ExamConstatnt {
+  const ExamConstatnt._();
   static const String singleChoice = "single_choice";
   static const String multipleChoice = "multiple_choice";
   static const String trueFalse = "true_false";
   static const String shortAnswer = "short_answer";
   static const String longAnswer = "long_answer";
   static const String compare = "compare";
+  static const String typeAssessmentAfterFinish = "after_finish";
+  static const String typeAssessmenDirect = "direct";
+  static const String choiceExam = "choice";
+  static const String randomlyExam = "randomly";
+  static const String aiExam = "ai";
 }
